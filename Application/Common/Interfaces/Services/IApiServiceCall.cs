7e0c1dab6eeceff003f54261ce61cc535b794d2f
Quentin Goeury <qgoeury-ext@goyard.com>
2025-06-27 12:46:48 +0200

    add apiKey + interface

---
+++ Application/Common/Interfaces/Services/IApiServiceCall.cs
@@ -0,0 +1,8 @@
+﻿
+
+namespace Application.Common.Interfaces.Services;
+
+public interface IApiServiceCall<T> where T : class
+{
+    Task<T> GetAsync(string id);
+}
--- Infrastructure/Services/ApiServiceCall.cs
+++ Infrastructure/Services/ApiServiceCall.cs
@@ -1,19 +1,34 @@
-﻿using Flurl.Http;
+﻿using Application.Common.Interfaces.Services;
+using Flurl;
+using Flurl.Http;
 
 namespace Infrastructure.Services;
 
-public class ApiServiceCall<T> where T : class, new()
+public class ApiServiceCall<T> : IApiServiceCall<T> where T : class, new()
 {
     private readonly string _apiBaseUrl;
+    private readonly string _apiKey;
 
-    public ApiServiceCall(string apiBaseUrl)
+    public ApiServiceCall(string apiBaseUrl, string apiKey)
     {
         _apiBaseUrl = apiBaseUrl;
+        _apiKey = apiKey;
     }
 
-    public async Task<T> GetAsync(int id)
+    public async Task<T> GetAsync(string id)
     {
-        return await $"{_apiBaseUrl}/{typeof(T).Name.ToLower()}/{id}"
-            .GetJsonAsync<T>();
+        return await GetUri(id).GetJsonAsync<T>();
+    }
+
+    private string GetUri(string id)
+    {
+        return _apiBaseUrl
+        .AppendPathSegment(id)
+        .SetQueryParams(new
+        {
+            api_key = _apiKey,
+            max_results = 20,
+        })
+        .SetFragment("after-hash");
     }
 }
