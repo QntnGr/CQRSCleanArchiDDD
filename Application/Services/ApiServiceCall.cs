02a46be5f876de9061ec445ed1701b457619d1cb
Quentin Goeury <qgoeury-ext@goyard.com>
2025-06-27 11:44:52 +0200

    set up generic sevice

---
+++ Application/Services/ApiServiceCall.cs
@@ -0,0 +1,19 @@
+﻿using Flurl.Http;
+
+namespace Application.Services;
+
+public class ApiServiceCall<T> where T : class, new()
+{
+    private readonly string _apiBaseUrl;
+
+    public ApiServiceCall(string apiBaseUrl)
+    {
+        _apiBaseUrl = apiBaseUrl;
+    }
+
+    public async Task<T> GetAsync(int id)
+    {
+        return await $"{_apiBaseUrl}/{typeof(T).Name.ToLower()}/{id}"
+            .GetJsonAsync<T>();
+    }
+}
