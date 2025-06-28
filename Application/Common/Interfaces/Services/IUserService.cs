887d33ce4201ff398569cc999f1eaeb2d0a75b7b
Quentin Goeury <qgoeury@gmail.com>
2025-06-28 18:15:23 +0200

    change user entity + set up userservice

---
+++ Application/Common/Interfaces/Services/IUserService.cs
@@ -0,0 +1,9 @@
+﻿
+using Domain.Entities;
+
+namespace Application.Common.Interfaces.Services;
+
+public interface IUserService
+{
+    Task<User> AuthenticateUser(string username, string password);
+}
---
+++ Infrastructure/Services/UserService.cs
@@ -0,0 +1,35 @@
+﻿
+
+using Application.Common.Interfaces.Persistance;
+using Application.Common.Interfaces.Services;
+using Domain.Entities;
+
+namespace Infrastructure.Services;
+
+public class UserService : IUserService
+{
+    private readonly IUserRepository _userRepository;
+
+    public UserService(IUserRepository userRepository)
+    {
+        _userRepository = userRepository;
+    }
+
+    public async Task<User> AuthenticateUser(string username, string password)
+    {
+        var user = await _userRepository.FindOneAsync(user => user.Name == username);
+
+        if (user == null || !VerifyPassword(password, user.Password))
+        {
+            return null;
+        }
+
+        return user;
+    }
+
+    private bool VerifyPassword(string inputPassword, string storedPassword)
+    {
+        // logique de vérification du mot de passe, BCrypt ou autre algorithme de hachage
+        return inputPassword == storedPassword;
+    }
+}
